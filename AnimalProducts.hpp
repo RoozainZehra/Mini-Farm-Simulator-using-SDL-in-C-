7bc0f9ec0277ae62c0fe2f13ac3c22df7cdd3493
AmnahQ <122616031+AmnahQ@users.noreply.github.com>
2023-11-12 23:16:11 +0500

    Add files via upload

---
+++ AnimalProducts.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class AnimalProducts{
+    private:
+    int num_eggs;
+    int num_milk;
+    public:
+    void store_animal_products(string Animal, int qty);
+    void display();  
+};
