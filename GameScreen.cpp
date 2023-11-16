c8e60355af416a8aa9ead9b783f6a6f15420d582
Hiba Shahid <122306826+hibx@users.noreply.github.com>
2023-11-16 18:12:11 +0500

    Update GameScreen.cpp

--- GameScreen.cpp
+++ GameScreen.cpp
@@ -6,7 +6,7 @@
 class Game {
     // Screen dimension constants
     const int SCREEN_WIDTH = 1100;
-    const int SCREEN_HEIGHT = 700;
+    const int SCREEN_HEIGHT = 650;
 
     // The window we'll be rendering to
     SDL_Window* gWindow = NULL;
@@ -25,8 +25,15 @@ class Game {
     SDL_Texture* barnTexture = NULL;
     //Texture for market
     SDL_Texture* marketTexture = NULL;
-    //Texture for Trees
-    SDL_Texture* treesTexture = NULL;
+    //Texture for Trees1 and Trees2
+    SDL_Texture* trees1Texture = NULL;
+    SDL_Texture* trees2Texture = NULL;
+    //Texture for Farm Patch
+    SDL_Texture* patchTexture = NULL;
+    //Texture for fence
+    SDL_Texture* fenceTexture = NULL;
+    //Texture for locks
+    SDL_Texture* lockTexture = NULL;
 
     //farmers dimensions
     const int farmer_ht = 90;
@@ -34,23 +41,39 @@ class Game {
     int farmer_x = 0;
     int farmer_y = 0;
 
-    //barn dimensions and initial position
+    //barn dimensions and position
     const int barn_ht = 318;
     const int barn_wdt = 312;
     const int barn_x = 354;
     const int barn_y = -30;
 
-    //market dimensions and initial position
-    const int market_ht = 180;
-    const int market_wdt = 150;
+    //market dimensions and position
+    const int market_ht = 120;
+    const int market_wdt = 90;
     const int market_x = 672;
-    const int market_y = 56;
+    const int market_y = 86;
 
-    //trees dimensions and initial position
+    //trees dimensions and position
     const int tree_ht = 184;
     const int tree_wdt = 177;
-    const int tree_x = 940;
+    const int tree_x = 955;
     // const int tree_y = 514;
+
+    //Land patch dimensions and position
+    const int patch_ht = 200;
+    const int patch_wdt = 200;
+    const int patch_x = 416;
+    const int patch_y = 273;
+
+    //animal fence dimensions and position
+    const int fence_ht = 120;
+    const int fence_wdt = 210;
+    const int fence_x = 210;
+    const int fence_y = 97;
+
+    //lock dimensions
+    const int lock_ht = 20;
+    const int lock_wdt = 15;
     
 public:
     bool init() {
@@ -90,41 +113,79 @@ public:
             return false;
         }
         //Load farmer image
-        SDL_Surface* surface = IMG_Load("farmer.png");
-        if (surface == nullptr) {
+        SDL_Surface* farmerS = IMG_Load("farmer.png");
+        if (farmerS == nullptr) {
             return false;
         }
-        farmerTexture = SDL_CreateTextureFromSurface(gRenderer, surface);
+        farmerTexture = SDL_CreateTextureFromSurface(gRenderer, farmerS);
         if (gTexture == nullptr) {
             return false;
         }
 
         //Load barn image
-        SDL_Surface* surface2 = IMG_Load("barn.png");
-        if (surface2 == nullptr) {
+        SDL_Surface* barnS = IMG_Load("barn.png");
+        if (barnS == nullptr) {
             return false;
         }
-        barnTexture = SDL_CreateTextureFromSurface(gRenderer, surface2);
+        barnTexture = SDL_CreateTextureFromSurface(gRenderer, barnS);
         if (barnTexture == nullptr) {
             return false;
         }
 
-        //Load barn image
-        SDL_Surface* surface3 = IMG_Load("market.png");
-        if (surface3 == nullptr) {
+        //Load marketplace image
+        SDL_Surface* marketS = IMG_Load("store.png");
+        if (marketS == nullptr) {
             return false;
         }
-        marketTexture = SDL_CreateTextureFromSurface(gRenderer, surface3);
+        marketTexture = SDL_CreateTextureFromSurface(gRenderer, marketS);
         if (marketTexture == nullptr) {
             return false;
         }
 
-        //Load trees on the screen
-        SDL_Surface* surface4 = IMG_Load("3trees.png");
-        if (surface4 == nullptr) {
+        //Load trees1 and 2 on the screen
+        SDL_Surface* trees1S = IMG_Load("3trees.png");
+        if (trees1S == nullptr) {
             return false;
         }
-        treesTexture = SDL_CreateTextureFromSurface(gRenderer, surface4);
+        trees1Texture = SDL_CreateTextureFromSurface(gRenderer, trees1S);
+        if (gTexture == nullptr) {
+            return false;
+        }
+        SDL_Surface* trees2S = IMG_Load("3treesflip.png");
+        if (trees2S == nullptr) {
+            return false;
+        }
+        trees2Texture = SDL_CreateTextureFromSurface(gRenderer, trees2S);
+        if (gTexture == nullptr) {
+            return false;
+        }
+
+        //Load patch on the screen
+        SDL_Surface* patchS = IMG_Load("landPatch.jpg");
+        if (patchS == nullptr) {
+            return false;
+        }
+        patchTexture = SDL_CreateTextureFromSurface(gRenderer, patchS);
+        if (gTexture == nullptr) {
+            return false;
+        }
+
+        //Load fence on the screen
+        SDL_Surface* fenceS = IMG_Load("animalSpace.png");
+        if (fenceS == nullptr) {
+            return false;
+        }
+        fenceTexture = SDL_CreateTextureFromSurface(gRenderer, fenceS);
+        if (gTexture == nullptr) {
+            return false;
+        }
+
+        //Load lock on the patches on screen
+        SDL_Surface* lockS = IMG_Load("lock.png");
+        if (lockS == nullptr) {
+            return false;
+        }
+        lockTexture = SDL_CreateTextureFromSurface(gRenderer, lockS);
         if (gTexture == nullptr) {
             return false;
         }
@@ -223,6 +284,26 @@ public:
             // Render the image
             SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
+            //Render the patch image
+            SDL_Rect patchRect = { patch_x, patch_y, patch_wdt, patch_ht};
+            SDL_RenderCopy(gRenderer, patchTexture, NULL, &patchRect);
+
+            //Render the locks on the patches
+            SDL_Rect lock1Rect = {456, 312, lock_wdt, lock_ht};
+            SDL_RenderCopy(gRenderer, lockTexture, NULL, &lock1Rect);
+            SDL_Rect lock2Rect = {506, 312, lock_wdt, lock_ht};
+            SDL_RenderCopy(gRenderer, lockTexture, NULL, &lock2Rect);
+            SDL_Rect lock3Rect = {555, 312, lock_wdt, lock_ht};
+            SDL_RenderCopy(gRenderer, lockTexture, NULL, &lock3Rect);
+            SDL_Rect lock4Rect = {506, 362, lock_wdt, lock_ht};
+            SDL_RenderCopy(gRenderer, lockTexture, NULL, &lock4Rect);
+            SDL_Rect lock5Rect = {555, 362, lock_wdt, lock_ht};
+            SDL_RenderCopy(gRenderer, lockTexture, NULL, &lock5Rect);
+            SDL_Rect lock6Rect = {506, 412, lock_wdt, lock_ht};
+            SDL_RenderCopy(gRenderer, lockTexture, NULL, &lock6Rect);
+            SDL_Rect lock7Rect = {555, 412, lock_wdt, lock_ht};
+            SDL_RenderCopy(gRenderer, lockTexture, NULL, &lock7Rect);
+
             //Render the farmer image
             SDL_Rect farmerRect = { farmer_x, farmer_y, farmer_wdt, farmer_ht };
             SDL_RenderCopy(gRenderer, farmerTexture, NULL, &farmerRect);
@@ -236,18 +317,36 @@ public:
             SDL_RenderCopy(gRenderer, marketTexture, NULL, &marketRect);
 
             //Render the tree image
-            SDL_Rect treeRect1 = {tree_x, 100, tree_wdt, tree_ht};
-            SDL_RenderCopy(gRenderer, treesTexture, NULL, &treeRect1);
-            SDL_Rect treeRect2 = {tree_x, 250, tree_wdt, tree_ht};
-            SDL_RenderCopy(gRenderer, treesTexture, NULL, &treeRect2);
-            SDL_Rect treeRect3 = {tree_x, 400, tree_wdt, tree_ht};
-            SDL_RenderCopy(gRenderer, treesTexture, NULL, &treeRect3);
-            SDL_Rect treeRect4 = {tree_x, 550, tree_wdt, tree_ht};
-            SDL_RenderCopy(gRenderer, treesTexture, NULL, &treeRect4);
-            SDL_Rect treeRect5 = {tree_x, 700, tree_wdt, tree_ht};
-            SDL_RenderCopy(gRenderer, treesTexture, NULL, &treeRect5);
-            SDL_Rect treeRect6 = {tree_x, 850, tree_wdt, tree_ht};
-            SDL_RenderCopy(gRenderer, treesTexture, NULL, &treeRect6);
+            SDL_Rect treeRect1 = {tree_x, 60, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees1Texture, NULL, &treeRect1);
+            SDL_Rect treeRect2 = {tree_x, 190, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees2Texture, NULL, &treeRect2);
+            SDL_Rect treeRect3 = {tree_x, 320, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees1Texture, NULL, &treeRect3);
+            SDL_Rect treeRect4 = {tree_x, 450, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees2Texture, NULL, &treeRect4);
+            SDL_Rect treeRect5 = {tree_x, 580, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees1Texture, NULL, &treeRect5);
+            SDL_Rect treeRect6 = {tree_x, 710, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees2Texture, NULL, &treeRect6);
+
+            int newtree_x = -20;
+            SDL_Rect treeRect1f = {newtree_x, 60, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees1Texture, NULL, &treeRect1f);
+            SDL_Rect treeRect2f = {newtree_x, 190, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees2Texture, NULL, &treeRect2f);
+            SDL_Rect treeRect3f = {newtree_x, 320, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees1Texture, NULL, &treeRect3f);
+            SDL_Rect treeRect4f = {newtree_x, 450, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees2Texture, NULL, &treeRect4f);
+            SDL_Rect treeRect5f = {newtree_x, 580, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees1Texture, NULL, &treeRect5f);
+            SDL_Rect treeRect6f = {newtree_x, 710, tree_wdt, tree_ht};
+            SDL_RenderCopy(gRenderer, trees2Texture, NULL, &treeRect6f);
+
+            //Render the fence image
+            SDL_Rect fenceRect = { fence_x, fence_y, fence_wdt, fence_ht};
+            SDL_RenderCopy(gRenderer, fenceTexture, NULL, &fenceRect);
 
             // Update screen
             SDL_RenderPresent(gRenderer);
