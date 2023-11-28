33c5a9c609c2df77d0ff18a8abc3e58cfaed0e07
RoozainZehra <122306933+RoozainZehra@users.noreply.github.com>
2023-11-28 04:53:43 -0800

    Update Vegetables.cpp

--- Vegetables.cpp
+++ Vegetables.cpp
@@ -1,20 +1,31 @@
 #include "Vegetables.hpp"
 
-Vegetables::Vegetables(bool grow, int price, int time, bool harvest){
-    grown = grow;
-    selling_price = price;
-    growth_time = time;
-    harvestStatus = 0;
+void Vegetables::VegetableRenderer(){
+    SDL_RenderCopy(gRenderer, T, NULL, R);
 }
 
 Vegetables::~Vegetables(){
     cout << "Object deleted\n";
 }
 
-void Vegetables::SetGrown(){
-    grown = 1;
+bool Vegetables::loadMedia(){
+    if (T == NULL){
+        std::cerr << "Failed to load texture image!" << std::endl;
+        return false;
+    }
+    return true;
 }
-void Vegetables::SetHarvest(){
-    harvestStatus = 1;
+
+void Vegetables::setVegetables(bool grow, int price, int time, bool harvest){
+    grown = grow;
+    selling_price = price;
+    growth_time = time;
+    harvestStatus = 0;
 }
+// void Vegetables::SetGrown(){
+//     grown = 1;
+// }
+// void Vegetables::SetHarvest(){
+//     harvestStatus = 1;
+// }
 
