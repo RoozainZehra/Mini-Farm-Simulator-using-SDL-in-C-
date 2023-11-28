fa55ae03bf0931fd46e24715bf4b7bb8caa891aa
RoozainZehra <122306933+RoozainZehra@users.noreply.github.com>
2023-11-28 04:49:49 -0800

    Update Carrot.cpp

--- Carrot.cpp
+++ Carrot.cpp
@@ -1,22 +1,28 @@
 #include "Carrot.hpp"
 
-Carrot::Carrot() : Vegetables::Vegetables(0, 20, 100, 0){
+Carrot::Carrot( SDL_Renderer* gRenderer, int x){
+    Vegetables::setVegetables(0, 20, 100, 0);
+    TextureManager element(gRenderer);
+    T = element.loadTexture("sprout.png");
+    R = {x, 423, 30, 10}; 
     current_stage = 1;
 }
 
-Carrot::~Carrot(){}
-
-void Carrot::Sell(int qty){}
-void Carrot::Grow(){
-    if (current_stage < 3){ //since full growth stage is 3
-        current_stage += 1; //image changes with every update in stage
-    }
-    if (current_stage == 3){
-        Vegetables::SetGrown;
-    }
+Carrot::~Carrot(){
+    delete this;
 }
 
-void Carrot::Harvest(){} //change plot to empty
+// void Carrot::Sell(int qty){}
+// void Carrot::Grow(){
+//     if (current_stage < 3){ //since full growth stage is 3
+//         current_stage += 1; //image changes with every update in stage
+//     }
+//     if (current_stage == 3){
+//         Vegetables::SetGrown();
+//     }
+// }
+
+// void Carrot::Harvest(){} //change plot to empty
 
 
 
