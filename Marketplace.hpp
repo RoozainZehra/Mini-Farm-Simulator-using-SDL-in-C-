8033c29ef7d9d520fff23f3ecadd71d182c59cec
RoozainZehra <122306933+RoozainZehra@users.noreply.github.com>
2023-11-28 04:55:01 -0800

    Update Marketplace.hpp

--- Marketplace.hpp
+++ Marketplace.hpp
@@ -1,15 +1,32 @@
+#pragma once
 #include <iostream>
-#include <vector>
+// #include <vector>
+#include "sdlManager.hpp"
 using namespace std;
-#include "Offer_for_seeds.hpp"
-#include "Offer_for_coins.hpp"
 
 class Marketplace{
     private:
-    vector<Offer_for_seeds> SeedOffers;
-    vector<Offer_for_coins> CoinOffers;
+    //Texture for market
+    SDL_Texture* marketTexture = NULL;
+    //market dimensions and position
+    const int market_ht = 128;
+    const int market_wdt = 100;
+    const int market_x = 562;
+    const int market_y = 86;
+
+    SDL_Rect marketRect = {market_x, market_y, market_wdt, market_ht};
+
     public:
-    void Purchase(int gold_coins, int qty);
-    // void Purchase(int num_seeds, int qty); // needs to be connected
-    void ViewOffers();
+    Marketplace(SDL_Renderer* gRenderer);
+    ~Marketplace();
+
+    void marketRender(SDL_Renderer* gRenderer);
+    bool loadMedia();
+
+    SDL_Rect getSDLRect();
+
+
+    // void Purchase(int gold_coins, int qty);
+    // // void Purchase(int num_seeds, int qty); // needs to be connected
+    // void ViewOffers();
 };
