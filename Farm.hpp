d1b14b908eaa386b5ca4876bff3ae52d1a577810
Hiba Shahid <122306826+hibx@users.noreply.github.com>
2023-11-30 13:37:19 +0500

    Update Farm.hpp

--- Farm.hpp
+++ Farm.hpp
@@ -2,28 +2,16 @@
 
 #include <iostream>
 #include "sdlManager.hpp"
+using namespace std;
 
-class Farmer{
+class Farm{
     private:
-    const int farmer_ht = 40;
-    const int farmer_wdt = 35;
-    int farmer_x = 40;
-    int farmer_y = 0;
-    SDL_Texture* farmerTexture = NULL;
-    SDL_Rect farmerRect = { farmer_x, farmer_y, farmer_wdt, farmer_ht };
+    SDL_Texture* farmTexture = NULL;
     
     public:
-    Farmer(SDL_Renderer* gRenderer);
-    ~Farmer();
+    Farm(SDL_Renderer* gRenderer);
+    ~Farm();
 
-    void farmerRender(SDL_Renderer* gRenderer);
+    void farmRender(SDL_Renderer* gRenderer);
     bool loadMedia();
-    void movements(char x);
-    void out_movement(int sw, int sh);
-    SDL_Rect Get_SDlRECT();
-    // void HarvestVegetables();
-    // void PlantVegetables(string VegetableType);
-    // void FeedAnimals(string AnimalType);
-    // void CollectAnimalProducts(string AnimalType);
-    // void ExpandLandPatches(int num_patches);
 };
