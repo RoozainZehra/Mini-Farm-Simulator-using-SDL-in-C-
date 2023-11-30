88265f7b5b99f16f3f5a313851b6a1c0caa628d1
Hiba Shahid <122306826+hibx@users.noreply.github.com>
2023-11-30 12:28:36 +0500

    Update Vegetables.hpp

--- Vegetables.hpp
+++ Vegetables.hpp
@@ -1,19 +1,32 @@
+#pragma once
 #include <iostream>
 using namespace std;
+#include "sdlManager.hpp"
 
 class Vegetables{
-    private:
+    protected:
+    uint32_t creation_time;
+    uint32_t current_time;
     bool grown;
     int selling_price; //make constant
     int growth_time; //make constant
     bool harvestStatus;
     
+    SDL_Renderer* Renderer = NULL;
+    SDL_Texture* Texture = NULL;
+    SDL_Rect Rect;
+
     public:
-    Vegetables(bool grow, int price, int time, bool harvest);
+    
+    Vegetables(){}
+    bool loadMedia();
+    void VegetableRenderer();
     ~Vegetables();
-    virtual void Sell(int qty) = 0;
-    virtual void Grow() = 0;
-    virtual void Harvest() = 0;
-    void SetGrown();
-    void SetHarvest();
+    // virtual void Sell(int qty) = 0;
+    virtual void Grow(){}
+    // virtual void Harvest() = 0;
+    // void SetHarvest();
+    void setVegetables(bool grow, int price, int time, bool harvest);
+    virtual uint32_t timeElapsed()=0;
+    // uint32_t timeElapsed() = 0;
 };
