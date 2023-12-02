4fa469a858feb7f1537df8fb4088017abf2ddd2c
Hiba Shahid <122306826+hibx@users.noreply.github.com>
2023-12-02 23:20:34 +0500

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -43,77 +43,96 @@ void Game::close(){
 }
 
 
-void Game::run(){
+void Game::run() {
     bool quit = false;
     SDL_Event e;
-    Farm *farm = new Farm(gRenderer); //initialize farm
+    Farm *farm = new Farm(gRenderer);
     LandPatches *land = new LandPatches(gRenderer);
     Farmer *farmer = new Farmer(gRenderer);
     Storage *storage = new Storage(gRenderer);
     Marketplace *market = new Marketplace(gRenderer);
 
-    while (!quit){
-        while (SDL_PollEvent(&e) != 0){
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
                 quit = true;
             }
-            SDL_RenderClear(gRenderer);
-            farm->farmRender(gRenderer);
-            land->landRender(gRenderer);
-            land->patchRender(gRenderer);
-            
-            for (auto& vegies : v){     
-                uint32_t check = vegies->timeElapsed();
-                if (check >= 30000){
-                vegies->Grow();
-                }
-                vegies->VegetableRenderer();
-            }
-            
-            farmer->farmerRender(gRenderer);
-            storage ->storageRender(gRenderer);
-            market->marketRender(gRenderer);
-            if (!farm->loadMedia() || !land->loadMedia() || !storage->loadMedia() || !market->loadMedia() || !farmer->loadMedia()){
-                break;
-            }
-            else if (e.type == SDL_KEYDOWN) {
+
+            if (e.type == SDL_KEYDOWN) {
                 switch (e.key.keysym.sym) {
                     case SDLK_UP:
-                        farmer ->movements('U');
+                        farmer->movements('U');
                         break;
                     case SDLK_DOWN:
-                        farmer ->movements('D');
+                        farmer->movements('D');
                         break;
                     case SDLK_LEFT:
-                        farmer ->movements('L');
+                        farmer->movements('L');
                         break;
                     case SDLK_RIGHT:
-                        farmer ->movements('R');
+                        farmer->movements('R');
                         break;
                     default:
                         break;
                 }
-            }
-            else if(e.type == SDL_MOUSEBUTTONDOWN){
-                if(e.button.button == SDL_BUTTON_LEFT){
+            } else if (e.type == SDL_MOUSEBUTTONDOWN) {
+                if (e.button.button == SDL_BUTTON_LEFT) {
                     int mouseX, mouseY;
                     SDL_GetMouseState(&mouseX, &mouseY);
-                    std::cout<< "Mouse clicked at X= " <<mouseX << ", Y= "<<mouseY << std::endl;
-                }
-            }
-            vector<SDL_Rect> patches = land->GetSDLRect();
-            for (auto& patch : patches){
-                if (farmer->checkCollision(patch)){
-                    land->createVeg(gRenderer, v);
-                }
-            }
-            farmer->out_movement(SCREEN_WIDTH, SCREEN_HEIGHT);
+                    std::cout << "Mouse clicked at X= " << mouseX << ", Y= " << mouseY << std::endl;
 
-            // Update screen
-            SDL_Delay(50);
-            SDL_RenderPresent(gRenderer);
-            
+                    // Animals* cow = new Cow(gRenderer); // Example: Creating a cow
+                    // // cow->setPosition(mouseX, mouseY); // Set the position of the animal
+                    // a.push_back(cow); // Add the animal to the vector
+                }
+            }
         }
-        
+
+        // Clear the renderer
+        SDL_RenderClear(gRenderer);
+
+        // Rendering game elements
+        farm->farmRender(gRenderer);
+        land->landRender(gRenderer);
+        land->patchRender(gRenderer);
+        land->createVeg(gRenderer, v);
+
+        // farm->getAnimals(gRenderer, a);
+        for (auto& vegies : v) {
+            uint32_t check = vegies->timeElapsed();
+            if (check >= 30000 && check < 70000) {
+                vegies->Grow();
+            } else if (check >= 70000) {
+                vegies->Grow();
+            }
+            vegies->VegetableRenderer();
+        }
+
+        // Render animals after updating positions
+        for (auto& animal : a) {
+            uint32_t check = (SDL_GetTicks() - animal->creation_time) % 10;
+            if (check == 0){
+                animal->walk();
+            }
+            animal->AnimalRenderer();
+        }
+
+        farmer->farmerRender(gRenderer);
+        storage->storageRender(gRenderer);
+        market->marketRender(gRenderer);
+
+        farmer->out_movement(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        // Update screen
+        SDL_RenderPresent(gRenderer);
+
+        SDL_Delay(50); // Add a small delay for better control of frame rate
     }
+
+    // Cleanup (deallocate memory, etc.) before exiting the game
+    delete farm;
+    delete land;
+    delete farmer;
+    delete storage;
+    delete market;
 }
