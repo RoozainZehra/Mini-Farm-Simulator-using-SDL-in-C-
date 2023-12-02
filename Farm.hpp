50c154dd0cd74dfc67c37fd275c6159761128c20
RoozainZehra <122306933+RoozainZehra@users.noreply.github.com>
2023-12-02 01:45:18 -0800

    Update Farm.hpp

--- Farm.hpp
+++ Farm.hpp
@@ -7,11 +7,18 @@ using namespace std;
 class Farm{
     private:
     SDL_Texture* farmTexture = NULL;
-    
+    SDL_Texture* fenceTexture = NULL;
+    SDL_Rect fenceRect;
+
     public:
     Farm(SDL_Renderer* gRenderer);
     ~Farm();
 
     void farmRender(SDL_Renderer* gRenderer);
     bool loadMedia();
+    // void HarvestVegetables();
+    // void PlantVegetables(string VegetableType);
+    // void FeedAnimals(string AnimalType);
+    // void CollectAnimalProducts(string AnimalType);
+    // void ExpandLandPatches(int num_patches);
 };
