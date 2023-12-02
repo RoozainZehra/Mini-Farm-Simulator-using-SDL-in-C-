0446f9f18187ed45abadf46d01f8e736a6c1c2d4
Hiba Shahid <122306826+hibx@users.noreply.github.com>
2023-12-02 12:31:42 +0500

    Update Wheat.cpp

--- Wheat.cpp
+++ Wheat.cpp
@@ -1,18 +1,44 @@
 #include "Wheat.hpp"
 
-Wheat::Wheat() : Vegetables::Vegetables(0, 10, 70, 0){
+Wheat::Wheat( SDL_Renderer* gRenderer, int x){
+    Vegetables::setVegetables(0, 20, 100, 0);
+    TextureManager element(gRenderer);
+    Vegetables::Renderer = gRenderer;
+    Vegetables::Texture = element.loadTexture("seedsPlanted.png");
+    Vegetables::Rect = {x+10, 426, 30, 42}; 
+
     current_stage = 1;
+    creation_time = SDL_GetTicks();  //time of creation
+    // std::cout << "seed planted" << std::endl;
 }
 
-Wheat::~Wheat(){}
-void Wheat::Sell(int qty){} //check at marketplace
+Wheat::~Wheat(){
+    delete this;
+}
+
+// void Carrot::Sell(int qty){}
 void Wheat::Grow(){
-    if (current_stage < 3){
-        current_stage += 1; //image changes at every increment
+    if (current_stage < 3){ //since full growth stage is 3
+        current_stage += 1; //image changes with every update in stage
+        if (current_stage == 2){
+            TextureManager element(Renderer);
+            Vegetables::Texture = element.loadTexture("sprout.png");
+            // std::cout << "sprout grown" << std::endl;
+        }
     }
-    else if (current_stage == 3){
-        Vegetables::SetGrown();
-        Vegetables::SetHarvest();
+    if (current_stage == 3 && timeElapsed()>=70000){
+        TextureManager element(Renderer);
+        Vegetables::Texture = element.loadTexture("wheatPlant.png"); 
+        // std::cout << "carrot grown" << std::endl;
+        Vegetables::grown = 1;
     }
 }
-void Wheat::Harvest(){} //connect to patches
+
+uint32_t Wheat::timeElapsed(){
+    current_time = SDL_GetTicks();
+    return current_time - creation_time;
+}
+
+// void Carrot::Harvest(){} //change plot to empty
+
+
