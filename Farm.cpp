6f11cd285c40db2305600b4b3c7de6f6457a4098
RoozainZehra <122306933+RoozainZehra@users.noreply.github.com>
2023-12-02 01:44:29 -0800

    Update Farm.cpp

--- Farm.cpp
+++ Farm.cpp
@@ -2,15 +2,19 @@
 
 Farm::Farm(SDL_Renderer* gRenderer){
     TextureManager element(gRenderer);
-    farmTexture = element.loadTexture("grass2.png");    
+    farmTexture = element.loadTexture("grass2.png");
+    TextureManager element_2(gRenderer);    
+    fenceTexture = element_2.loadTexture("fence.png");
 }
 
 void Farm::farmRender(SDL_Renderer* gRenderer){
     SDL_RenderCopy(gRenderer, farmTexture, NULL, NULL);
+    fenceRect = {470, 300, 310, 320};
+    SDL_RenderCopy(gRenderer, fenceTexture, NULL, &fenceRect);
 }
 
 bool Farm::loadMedia(){
-    if (farmTexture == NULL){
+    if (farmTexture == NULL || fenceTexture == NULL){
         std::cerr << "Failed to load texture image!" << std::endl;
         return false;
     }
