e16adbc8c146409e3b8b086ae8863fe4dbd6ea85
Hiba Shahid <122306826+hibx@users.noreply.github.com>
2023-12-07 14:09:01 +0500

    Update Storage.cpp

--- Storage.cpp
+++ Storage.cpp
@@ -7,6 +7,14 @@ Storage::Storage(SDL_Renderer* gRenderer){
     trees1Texture = elementL.loadTexture("treesLeft.png");
     TextureManager elementR(gRenderer);
     trees2Texture = elementR.loadTexture("treesRight.png");
+
+    GoldCoins = 150;
+    carrots = 1;
+    wheat = 1;
+    radish = 1;
+    milk = 0;
+    eggs = 0;
+    seeds = 4;
 }
 
 Storage::~Storage(){
@@ -34,4 +42,70 @@ SDL_Rect Storage::getSDLRect(){
     return storageRect;
 }
 
+void Storage::addMaterial(char Material, int qty){
+    if (Material == 'M')
+        milk += qty;
+    else if (Material == 'E')
+        eggs += qty;
+    else if (Material == 'C')
+        carrots += qty;
+    else if (Material == 'R')
+        radish += qty;
+    else if (Material == 'W')
+        wheat += qty;
+    else
+        GoldCoins += qty;
 
+}
+
+int Storage::getAmount(char Material){
+    if (Material == 'M')
+        return milk;
+    else if (Material == 'E')
+        return eggs;
+    else if (Material == 'C')
+        return carrots;
+    else if (Material == 'R')
+        return radish;
+    else if (Material == 'W')
+        return wheat;
+    else
+        return GoldCoins;
+
+
+}
+
+bool Storage::init() {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    // Create window
+    StorageWindow = SDL_CreateWindow("Storage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (StorageWindow == NULL) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    // Create renderer
+    StorageRenderer = SDL_CreateRenderer(StorageWindow, -1, SDL_RENDERER_ACCELERATED);
+
+    if (StorageRenderer == NULL) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    // Set renderer color
+    SDL_SetRenderDrawColor(StorageRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    return true;
+}
+
+void Storage::openStorage(){
+    if (!init()){
+        std::cerr << "Failed to initialize or load media!" << std::endl;
+    }
+}
