602ea145feec6f4a138647cf67eaa23866ef969c
Hiba Shahid <122306826+hibx@users.noreply.github.com>
2023-12-07 14:12:13 +0500

    Update Vegetables.hpp

--- Vegetables.hpp
+++ Vegetables.hpp
@@ -12,21 +12,21 @@ class Vegetables{
     int growth_time; //make constant
     bool harvestStatus;
     
-    SDL_Renderer* Renderer = NULL;
-    SDL_Texture* Texture = NULL;
-    SDL_Rect Rect;
+    SDL_Renderer* Renderer = NULL;  //window renderer
+    SDL_Texture* Texture = NULL;    //vegetable texture unique to each vegetable
+    SDL_Rect Rect;                  //unique vegetable dimensions for each
 
     public:
     
-    Vegetables(){}
+    Vegetables();
+    virtual ~Vegetables();
+    
     bool loadMedia();
     void VegetableRenderer();
-    ~Vegetables();
     // virtual void Sell(int qty) = 0;
     virtual void Grow(){}
     // virtual void Harvest() = 0;
-    // void SetHarvest();
     void setVegetables(bool grow, int price, int time, bool harvest);
+    SDL_Rect getRect();
     virtual uint32_t timeElapsed()=0;
-    // uint32_t timeElapsed() = 0;
 };
