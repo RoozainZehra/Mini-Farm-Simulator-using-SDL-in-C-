6b32f02a2583935bad1c0cea5b8b09161548c6d4
AmnahQ <122616031+AmnahQ@users.noreply.github.com>
2023-12-20 02:17:05 +0500

    Update Carrot.hpp

--- Carrot.hpp
+++ Carrot.hpp
@@ -1,1 +1,20 @@
+#pragma once
 
+#include <iostream>
+using namespace std;
+#include "Vegetables.hpp"
+
+class Carrot:public Vegetables{
+    private:
+    int current_stage;
+
+
+    public:
+    Carrot(SDL_Renderer* gRenderer, int x);
+    ~Carrot();
+    uint32_t timeElapsed();
+    void Grow();
+    char Type();
+    int Harvest();
+    bool isHarvestable();
+};
