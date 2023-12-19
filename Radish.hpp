ecb62a7982de6688f87e534663b26e993af2c1d2
AmnahQ <122616031+AmnahQ@users.noreply.github.com>
2023-12-20 02:18:08 +0500

    Update Radish.hpp

--- Radish.hpp
+++ Radish.hpp
@@ -1,1 +1,20 @@
+#pragma once
 
+#include <iostream>
+using namespace std;
+#include "Vegetables.hpp"
+
+class Radish:public Vegetables{
+    private:
+    int current_stage;
+
+
+    public:
+    Radish(SDL_Renderer* gRenderer, int x);
+    ~Radish();
+    uint32_t timeElapsed();
+    void Grow();
+    char Type();
+    int Harvest();
+    bool isHarvestable();
+};
